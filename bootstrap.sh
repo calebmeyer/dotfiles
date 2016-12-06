b444c50fb9fc5ce074fe2810a98f595d811ed471
Caleb Meyer <caleb.meyer@cerner.com>
2016-12-06 12:31:03 -0600

    Added pyenv and dependencies.

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,7 +4,7 @@
 
 echo "Installing essentials from aptitude..."
 sudo apt install -y curl fish zsh emacs vim build-essential git tree rbenv libssl-dev libreadline-dev zlib1g-dev \
-libmysqlclient-dev libsqlite3-dev silversearcher-ag
+     libmysqlclient-dev libsqlite3-dev silversearcher-ag libncurses5 libncurses5-dev libncursesw5
 
 echo "Cloning dotfiles..."
 git clone https://github.com/calebmeyer/dotfiles.git ~/.dotfiles
@@ -12,6 +12,9 @@ git clone https://github.com/calebmeyer/dotfiles.git ~/.dotfiles
 echo "Cloning rbenv and rbenv install..."
 git clone https://github.com/rbenv/rbenv.git ~/.rbenv
 git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
+
+echo "Installing pyenv"
+curl -L https://raw.githubusercontent.com/yyuu/pyenv-installer/master/bin/pyenv-installer | bash
 
 echo "Setting up dotfiles..."
 sudo chmod +x ~/.dotfiles/install
@@ -31,8 +34,7 @@ echo "Setting up fish..."
 curl -L http://get.oh-my.fish | fish
 # currently you have to type exit after the above step ^^
 fish -c "omf install rbenv"
+fish -c "omf install pyenv"
 fish -c "omf install bobthefish"
 
 echo "Finished."
-
-
