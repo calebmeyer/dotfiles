b84a6f2f050b225288638cfcc92e99e60ebb8c11
Caleb Meyer <caleb.meyer@cerner.com>
2017-04-05 10:01:05 -0500

    Updated k for newer antigen.

--- fish/functions/k.fish
+++ fish/functions/k.fish
@@ -1,3 +1,3 @@
 function k
-	zsh -c ". /home/caleb/.antigen/repos/https-COLON--SLASH--SLASH-github.com-SLASH-rimraf-SLASH-k.git/k.sh; k"
+	zsh -c ". /home/caleb/.antigen/bundles/rimraf/k/k.sh; k"
 end
