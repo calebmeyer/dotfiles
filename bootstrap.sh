f62a5c722e61660b59cd02a7dea029dc8a4dd1c4
Caleb Meyer <caleb.meyer@cerner.com>
2017-04-18 12:09:00 -0500

    Hardened bootstrap script.

--- bootstrap.sh
+++ bootstrap.sh
@@ -25,6 +25,10 @@ git clone https://github.com/calebmeyer/dotfiles.git ~/.dotfiles
 echo "Cloning rbenv and rbenv install..."
 git clone https://github.com/rbenv/rbenv.git ~/.rbenv
 git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
+if ! echo $PATH | grep -q "rbenv"
+then
+  echo 'export PATH=$HOME/.rbenv/bin:$PATH >> ~/.bashrc'
+fi
 
 echo "Installing pyenv"
 curl -L https://raw.githubusercontent.com/yyuu/pyenv-installer/master/bin/pyenv-installer | bash
@@ -33,9 +37,12 @@ echo "Setting up dotfiles..."
 sudo chmod +x ~/.dotfiles/install
 fish ~/.dotfiles/install
 
-echo "Installing source code pro..."
-sudo chmod +x ~/.dotfiles/install_source_code_pro.sh
-sh ~/.dotfiles/install_source_code_pro.sh
+if ! fc-list | grep -iq "Source Code Pro"
+then
+  echo "Installing source code pro..."
+  sudo chmod +x ~/.dotfiles/install_source_code_pro.sh
+  sh ~/.dotfiles/install_source_code_pro.sh
+fi
 
 echo "Installing Spacemacs..."
 git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
@@ -44,11 +51,17 @@ echo "Installing Vundle..."
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 echo "Setting up fish..."
-curl -L http://get.oh-my.fish | fish
-# currently you have to type exit after the above step ^^
-fish -c "omf install rbenv"
-fish -c "omf install pyenv"
-fish -c "omf install bobthefish"
-fish -c "omf install bang-bang"
+if [ ! -d ~/.config/omf ]
+then
+  curl -L http://get.oh-my.fish | fish
+fi
+
+echo "Changing shell to fish..."
+sudo usermod -s `which fish` $USER
+
+if ! fish -c "omf list" | grep -iq bobthefish
+then
+  fish -c "omf install rbenv pyenv bobthefish bang-bang"
+fi
 
 echo "Finished."
