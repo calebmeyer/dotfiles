f96b4245bd287b504a7ed83f44791d0b91b8206f
Caleb Meyer <Kiaulen@gmail.com>
2021-06-07 15:39:30 -0500

    Add procs

--- bootstrap.sh
+++ bootstrap.sh
@@ -35,7 +35,7 @@ then
 
   echo "Installing essentials from brew"
   brew tap homebrew/cask-fonts
-  brew install fish emacs neovim git tree ripgrep exa bat mysql wget font-hack font-source-code-pro font-hack \
+  brew install fish emacs neovim git tree ripgrep exa bat mysql wget font-hack font-source-code-pro procs \
                iterm2 firefox keepingyouawake bettertouchtool alfred
 elif [ "$OS" == "Linux" ]
 then
