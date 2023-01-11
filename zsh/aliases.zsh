908a2aaa73495c2a6a3c1235612380685d4dc642
Caleb Meyer <caleb.meyer@hy-vee.com>
2023-01-11 12:49:40 -0600

    Updated zshrc and aliases for the modern age

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -4,47 +4,9 @@
 alias reconfigure="$EDITOR ~/.zshrc"
 alias aliases="$EDITOR ~/.aliases.zsh"
 alias reload_aliases="source ~/.aliases.zsh"
-alias reload_theme="rm -rf .antigen/repos/https-COLON--SLASH--SLASH-github.com-SLASH-calebmeyer-SLASH-cpm-zsh-theme.git/ ; antigen theme calebmeyer/cpm-zsh-theme cpm"
-
-# go to folder aliases
-alias hd="cd ~/ruby_workspace/haberdashery/haberdasher"
-alias hds="cd ~/ruby_workspace/haberdashery/haberdasher_service"
-alias ssc="cd ~/ruby_workspace/services/semantic_service_client"
-alias ha="cd ~/ruby_workspace/haberdashery/healtheanalytics"
-alias hedw="cd ~/ruby_workspace/haberdashery/healtheedw"
-alias monocle="cd ~/ruby_workspace/clients/monocle"
-alias ms="cd ~/ruby_workspace/services/monocle_service"
-alias ss="cd ~/ruby_workspace/services/semantic_service"
-alias sfs="cd ~/ruby_workspace/services/storefront_service"
-alias laptop="cd ~/personal/laptop"
-alias msc="cd ~/chef_workspace/monocle_service_cookbook"
-alias secrets="cd ~/chef_workspace/secrets_chef"
-alias rcdash="cd ~/ruby_workspace/other_teams/rev_cycle_dashboard"
-alias sf="cd ~/ruby_workspace/clients/storefront"
-alias theme="cd ~/personal/cpm-zsh-theme"
-alias rust_workspace="cd ~/personal/rust_workspace"
-alias bletchley="cd ~/chef_workspace/bletchley"
-alias cmd="cd ~/ruby_workspace/other_teams/command_center"
-alias ds="cd ~/personal/dev_setup"
-alias tw="cd ~/ruby_workspace/clients/tabwhoa"
-alias d="cd ~/ruby_workspace/clients/dossier"
-
-# ruby aliases
-alias rs="rake site"
-alias rr="bundle exec rspec"
-alias rit="ruby spec/integration/integration.rb"
-alias rake_clean_db="rake db:drop db:create db:migrate db:reset"
-alias bi="bundle install"
-alias bu="bundle update"
-alias asset_clear="rake assets:clobber && rake tmp:clear"
-alias open_lint="open target/site/Lint-Results/lint-results.html"
-alias coverage="open target/site/Code-Coverage/index.html"
-alias site_index="open target/site/index.html"
-alias kill_rails='kill "$(lsof -i :3000 -t)"'
-alias tags='ctags -R --languages=ruby --exclude=.git --exclude=log .'
 
 # git aliases
-alias master="git checkout master"
+alias master="git checkout master; git pull"
 alias last_diff="git diff HEAD~..HEAD"
 alias diff_with_master="git diff master..HEAD"
 alias reset="git reset --hard"
@@ -60,9 +22,9 @@ alias pickmeup="fortune | cowsay | lolcat"
 alias duu="~/java_workspace/data-upload-1.0.2/bin/upload"
 alias oozie="~/java_workspace/oozie-client-2.3.2-cdh3u6/bin/oozie"
 
-alias ls="k"
-alias ll="k"
-alias la="k -a"
+alias ls="exa"
+alias ll="exa -l"
+alias la="exa -a"
 
 # general goodies
 mkcd () { mkdir $1 && cd $1; }
