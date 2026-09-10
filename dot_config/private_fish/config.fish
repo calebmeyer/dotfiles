428b06a25ba32d7a618b8646c84099b8943c2fbd
Caleb Meyer <caleb.meyer@hy-vee.com>
2026-09-10 11:35:43 -0500

    Sync config.fish, fish_plugins, and fish_variables with live state
    config.fish had drifted from an earlier uncommitted chezmoi add; this
    captures what's actually running (aliases, OBJC env var, conditional
    iTerm integration). fish_plugins/fish_variables were already staged
    and matched live state, just hadn't been committed yet.

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -1,9 +1,21 @@
 if status is-interactive
-    # Commands to run in interactive sessions can go here
-	function last_history_item; echo $history[1]; end 
+    # plugin for bang bang support
+	function last_history_item; echo $history[1]; end
 	abbr -a !! --position anywhere --function last_history_item
 
 	starship init fish | source
 	zoxide init fish | source
 	direnv hook fish | source
+	if test "$TERM_PROGRAM" = "iTerm.app"
+        source ~/.config/fish/.iterm2_shell_integration.fish
+    end
+
+    # https://github.com/rails/rails/issues/38560#issuecomment-590236052
+    # Fixes an issue with running native rails extensions (like solid_queue)
+    set -x OBJC_DISABLE_INITIALIZE_FORK_SAFETY yes
+
+    alias cat bat
+    alias ps procs
+    alias dps oxker # docker ps
+    alias ls eza
 end
